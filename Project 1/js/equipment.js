07646d15abf9603076b4465f0905cea1286846a4
Anja <anjsto@gmail.com>
2015-09-03 10:58:42 -0600

    initial project files due Friday

---
+++ Project 1/js/equipment.js
@@ -0,0 +1,29 @@
+var $ = function (id) {
+	return doument.getElementById(id);
+}
+
+$(document).ready(function() {
+	
+	// runs when an h2 heading is clicked
+	$("#equipmentBody h2").toggle(
+		function() {
+			$(this).toggleClass("minus");
+		    $(this).next().slideDown(1000, "easeOutBounce");
+	    },
+	    function() {
+	        $(this).toggleClass("minus");
+	        $(this).next().slideUp(1000, "easeInBounce");
+	    }
+    ); // end toggle	
+	
+	// runs when the page is ready
+	$("#equipmentBody h1").animate( { fontSize: "650%", opacity: 1, left: "+=375" }, 1000, "easeInExpo" )  
+		         .animate( { fontSize: "175%", left: "-=200" }, 1000, "easeOutExpo" );
+		    
+	// runs when the top-level heading is clicked
+	$("#equipmentBody h1").click(function() {
+		$(this).animate( { fontSize: "650%", opacity: 1, left: "+=375" }, 2000, "easeInExpo" )  
+			   .animate( { fontSize: "175%", left: 0 }, 1000, "easeOutExpo" );
+	}); // end click
+	
+}); // end ready
---
+++ Project 1/readme/readme.txt
@@ -0,0 +1,6 @@
+This project contains multiple files linked to the index.html. This is an informational web page about Photography including lessons and tutorials for beginners.
+
+Please find equipment.html, lessons.html, and tutorials.html, newsletter.html as well as stylesheet.css and Diana.jpg and newsletter.js.
+
+The css files are located at css/
+The images are located at images/
